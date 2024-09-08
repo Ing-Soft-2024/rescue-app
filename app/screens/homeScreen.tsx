159d2855c55661a3fc4d2cab881a6a53811864ce
nnicokim <nkim@itba.edu.ar>
2024-09-08 15:21:15 -0300

    Directory screens & screens files created

--- app/screens/homeScreen.tsx
+++ app/screens/homeScreen.tsx
@@ -1,18 +1,13 @@
 import { View, Text, Button } from 'react-native';
 import { useRouter } from 'expo-router';
 
-export default function company() {
+export default function homeScreen() {
     const router = useRouter();
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Company Profile</Text>
-          <Button title="Go to Home" onPress={() => router.push('../screens/HomeScreen')} />
+          <Button title="Go to company profile" onPress={() => router.push('./companyScreen')} />
         </View>
       );
+
 }
-
-
-
-
-
