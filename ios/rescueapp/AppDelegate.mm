8c34bcd109bfcacfddd583bea734c969675d1b27
Nicolas Kim <nkim@itba.edu.ar>
2024-09-23 13:08:46 -0300

    additional changes

--- ios/rescueapp/AppDelegate.mm
+++ ios/rescueapp/AppDelegate.mm
@@ -3,6 +3,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
