3d93ef2472b0d0e4b71348d6e81abbc7e30f17fd
Nicolas Kim <nkim@itba.edu.ar>
2024-10-10 13:34:13 -0300

    2 test files added

---
+++ app/(screens)/__tests__/index-test.tsx
@@ -0,0 +1,16 @@
+// screen tests
+
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+
+import { render } from '@testing-library/react-native';
+
+import HomeScreen, { } from '@/app/(screens)/index';
+
+import ProductLayout, { } from '@/app/(screens)/productScreen';
+
+describe('<HomeScreen />', () => {
+    it('renders correctly', () => {
+        render(<HomeScreen />);
+    });
+});
---
+++ src/__tests__/src-test.tsx
@@ -0,0 +1,6 @@
+// src tests
+
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+
+import { render } from '@testing-library/react-native';
