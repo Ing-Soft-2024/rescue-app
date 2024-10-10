d8f48cf40b4d6f4738670976ad75268385ea4cf8
juane993 <jolivamorroni@itba.edu.ar>
2024-10-10 20:05:44 -0300

    trying to fix testing

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+            "root": [
+                "./"
+            ],
+            "alias": {
+                "@": "./",
+                "@/src": "./src",
+                "@/components": "./src/components",
+                "@/context": "./src/context",
+                "@/auth": "./src/services/auth",
+            }
+        }
+    ],
+    ]
   };
 };
