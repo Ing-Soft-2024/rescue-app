bb5a0dbaa8ed50266828d863e19aed3d6c4f8899
juane993 <jolivamorroni@itba.edu.ar>
2024-10-12 18:43:47 -0300

    ver QR de la orden

--- app/(screens)/index.tsx
+++ app/(screens)/index.tsx
@@ -2,18 +2,24 @@ import { useSession } from '@/src/context/session.context';
 import { productConsumer } from '@/src/services/client';
 import { ProductType } from '@/src/types/product.type';
 import React from 'react';
-import { Button, FlatList, View } from 'react-native';
+import { Button, FlatList, View, Text, Pressable } from 'react-native';
 import { CategoryList } from '../../components/CategoryList';
 import { SearchBar } from '../../components/SearchBar';
+import { useOrders } from '@/src/context/ordersContext';
+import { router } from 'expo-router';
+import { colors } from '@/src/global-style';
 
 export default function homeScreen() {
   const { signOut } = useSession();
 
   const [category, setCategory] = React.useState<ProductType[]>([]);
+
   React.useEffect(() => {
     productConsumer.consume('GET')
       .then(setCategory);
   }, [])
+
+  const { cart, total } = useOrders();
 
   const sampleProduct: ProductType = {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
@@ -22,12 +28,22 @@ export default function homeScreen() {
     price: 100,
     image: ''
   };
+  const viewOrder = () => {
+    // router.push("./(checkout)/mercadoPago");
+    router.push("./QRScreen");
+ }
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <View style={{ paddingHorizontal: 10 }}>
         <SearchBar />
       </View>
+
+      {cart.length > 0 && 
+      <Pressable style={{padding: 10, backgroundColor: "#D4685E", borderRadius: 10}} onPress={viewOrder}>
+      <Text style={{fontSize: 20, color: 'white'}}>Presiona aqui para ver el QR de tu orden</Text>
+      </Pressable>}
+      
       <FlatList
         data={[1]}
         renderItem={({ item }) =>
