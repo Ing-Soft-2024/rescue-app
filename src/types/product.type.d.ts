86dc5918ac3489097a87d6981f4cbf7babb91d8c
juane993 <jolivamorroni@itba.edu.ar>
2024-12-13 16:24:05 -0300

    added product stock check and addToCartFix
    fixed bug that allows user to add a product to the cart if they have a currently running order.
    
    fixed bug that allows user to add more products to the cart than there are available

--- src/types/product.type.d.ts
+++ src/types/product.type.d.ts
@@ -5,6 +5,7 @@ export type ProductType = {
     price: number;
     image: string;
     category: string;
+    stock: number;
 }
 
 //el productScreen tiene un productType propio, y ese es el que se le agrega al carrito. no deberia usar este?
